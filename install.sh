b5a71ca04b102a2044b74baaa2d331c5e28dcf66
Michael Benford <michael.benford@corp.globo.com>
2017-09-19 13:40:24 -0300

    Remove color configuration from installation script

--- install.sh
+++ install.sh
@@ -2,28 +2,28 @@
 
 set -e
 
-echo -e "\e[33m=> Preparing to install...\e[0m"
+echo "=> Preparing to install..."
 
 if [ ! -x "$(command -v git)" ]; then
-    echo -e "\e[31merror: Git doesn't seem to be installed. Aborting..."
+    echo "error: Git doesn't seem to be installed. Aborting..."
     exit 1
 fi
 
 if [[ -z "${GOPATH}" ]]; then
-    echo -e "\e[31merror: GOPATH isn't set. Aborting..."
+    echo "error: GOPATH isn't set. Aborting..."
     exit 1
 fi
 
 install_path="${GOPATH}/src/github.com/globocom/prettylog"
 
 if [ -d "$install_path" ]; then
-    echo -e "\e[33m=> Updating to latest version...\e[0m"
+    echo "=> Updating to latest version..."
     (cd ${install_path} && git pull)
 else
-    echo -e "\e[33m=> Cloning repository into GOPATH...\e[0m"
+    echo "=> Cloning repository into GOPATH..."
     mkdir -p ${install_path}
     git clone git@github.com:globocom/prettylog.git ${install_path}
 fi
 
-echo -e "\e[33m=> Compiling and installing the application...\e[0m"
+echo "=> Compiling and installing the application..."
 (cd ${install_path} && make install)
