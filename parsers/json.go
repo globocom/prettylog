43500f2f5a0d60783144f4a82709da6bf76e49ad
raphael.rossi <raphael.rossi@engdb.com.br>
2021-10-04 17:50:57 -0300

    Change switch case to compare actual key with key array from yaml configuration file

--- parsers/json.go
+++ parsers/json.go
@@ -1,8 +1,8 @@
 package parsers
 
 import (
-	"github.com/tidwall/gjson"
 	"github.com/globocom/prettylog/config"
+	"github.com/tidwall/gjson"
 )
 
 type JsonLineParser struct{}
@@ -16,16 +16,16 @@ func (*JsonLineParser) Parse(line string) (*ParsedLine, error) {
 	parsed := &ParsedLine{}
 
 	gjson.Parse(line).ForEach(func(key, value gjson.Result) bool {
-		switch key.String() {
-		case settings.Timestamp.Key:
+		switch {
+		case containsKey(key.String(), settings.Timestamp.Key):
 			parsed.Timestamp = value.String()
-		case settings.Logger.Key:
+		case containsKey(key.String(), settings.Logger.Key):
 			parsed.Logger = value.String()
-		case settings.Caller.Key:
+		case containsKey(key.String(), settings.Caller.Key):
 			parsed.Caller = value.String()
-		case settings.Level.Key:
+		case containsKey(key.String(), settings.Level.Key):
 			parsed.Level = value.String()
-		case settings.Message.Key:
+		case containsKey(key.String(), settings.Message.Key):
 			parsed.Message = value.String()
 		default:
 			parsed.Fields = append(parsed.Fields, []string{key.String(), value.String()})
